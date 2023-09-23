9e2fde3d219edce183ded584b688d449c62e0403
Martin <martin-yin@foxmail.com>
2023-09-23 18:46:44 +0800

    feat: 增加 markdown 输出方式

--- src/plugins/modulesAnalysis.js
+++ src/plugins/modulesAnalysis.js
@@ -2,6 +2,7 @@ const fs = require('node:fs')
 const path = require('node:path')
 const swc = require('@swc/core')
 const vueCompiler = require('@vue/compiler-dom');
+const json2md = require('json2md')
 
 const pluginName = "ModulesAnalysis"
 
@@ -9,9 +10,9 @@ class ModulesAnalysis {
   constructor(options) {
     this.acceptType = options?.acceptType || ['vue', 'js', 'jsx', 'tsx', 'ts']
     this.ignoreModules = options?.ignoreModules || ['vue', 'vue-router']
+    this.outputType = options?.outputType || 'json'
     this.packageNodeModules = this.getPackageNodeModules();
   }
-
 
   getImportOrRequireName(value) {
     const regexp = /\//g;
@@ -75,7 +76,6 @@ class ModulesAnalysis {
         scriptCode = element.children[0].content;
       }
     });
-
     return scriptCode
   }
 
@@ -87,29 +87,28 @@ class ModulesAnalysis {
       script: true,
       target: 'es5',
       isModule: true,
+      tsx: true,
     });
-
     return body;
   }
 
   async getNodeModuleFilesMap(filePathList) {
+    const cwd = process.cwd() + '\\';
     const nodeModuleFilesMap = new Map();
     const nodeModulePromises = filePathList.map(async (filePath) => {
       const ast = await this.parseCodeToAstByFilePath(filePath);
       const nodeModules = this.getModules(ast).filter((module) => this.isNodeModule(module.value))
-
       if (nodeModules.length) {
         nodeModules.forEach((module) => {
           const { value, useType } = module;
           const nodeModuleFilesMapValue = nodeModuleFilesMap?.get(value) || [];
           nodeModuleFilesMap.set(value, [...nodeModuleFilesMapValue, {
-            filePath,
+            filePath: filePath.replace(cwd, ''),
             useType
           }]);
         })
       }
     });
-
     await Promise.all(nodeModulePromises);
     return nodeModuleFilesMap;
   }
@@ -135,13 +134,37 @@ class ModulesAnalysis {
     return this.acceptType.includes(filePath.split('.').pop())
   }
 
+  outputTypeResult(result) {
+    if (this.outputType === 'markdown') {
+      this.outputResultWithMarkdown(result)
+      return
+    }
+    this.outputResultByJson(result)
+  }
+
+  outputResultWithMarkdown(result) {
+    const markdownContent = []
+    result.forEach((item) => {
+      markdownContent.push({ h2: item.name })
+      markdownContent.push({ h3: `使用次数: ${item.total}` })
+      item.files.forEach(({ filePath, useType }) => {
+        markdownContent.push({ p: `文件: ${filePath}` })
+        markdownContent.push({ p: `使用方式：: ${useType}` })
+      })
+    })
+    fs.writeFileSync('./modulesAnalysis.md', json2md([{ h1: `${pluginName}分析结果` }, ...markdownContent]))
+  }
+
+  outputResultByJson(result) {
+    fs.writeFileSync('./modulesAnalysis.json', JSON.stringify(result))
+  }
+
   apply(compiler) {
     compiler.hooks.done.tap(pluginName, async (stats) => {
       const filesPath = [...stats.compilation.fileDependencies].filter((filePath) => this.isAcceptFile(filePath))
       const filesModuleMap = await this.getNodeModuleFilesMap(filesPath)
       const moduleUseInfo = this.calculateModuleUseInfo(filesModuleMap);
-
-      fs.writeFileSync('./modulesAnalysis.json', JSON.stringify(moduleUseInfo))
+      this.outputTypeResult(moduleUseInfo)
     })
   }
 }
--- src/plugins/safeDeleteFile.js
+++ src/plugins/safeDeleteFile.js
@@ -1,13 +1,15 @@
-const pluginName = 'SafeDeleteFile'
-
 const fs = require('node:fs')
 const path = require('node:path')
 const ignorejs = require('ignore')
+const json2md = require('json2md')
+
+const pluginName = 'SafeDeleteFile'
 
 class SafeDeleteFile {
     constructor(options = null) {
         this.folderPath = options?.folderPath || path.join(process.cwd(), '/src')
         this.ignore = ignorejs().add(['node_modules', '.git'].concat(options?.ignore || []))
+        this.outputType = options?.outputType || 'json'
         this.allFolderFiles = this.getAllFilesInFolder(this.folderPath)
     }
 
@@ -33,14 +35,34 @@ class SafeDeleteFile {
 
 
     getFilesDifference(allFolderFiles, afterCompileFiles) {
-        const differenceFiles = allFolderFiles.filter((item) => !afterCompileFiles.includes(item))
+        const cwd = process.cwd() + '\\';
+        const differenceFiles = allFolderFiles.filter((item) => !afterCompileFiles.includes(item)).map((filePath) => filePath.replace(cwd, ''))
         return differenceFiles
+    }
+
+    outputTypeResult(result) {
+        if (this.outputType === 'markdown') {
+            this.outputResultWithMarkdown(result)
+            return
+        }
+        this.outputResultWithJson(result)
+    }
+
+    outputResultWithJson(result) {
+        fs.writeFileSync('./safeDeleteFile.json', JSON.stringify(result))
+    }
+
+    outputResultWithMarkdown(result) {
+        const markdownContent = result.map((item) => {
+            return { p: `${item}` }
+        })
+        fs.writeFileSync('./safeDeleteFile.md', json2md([{ h1: `${pluginName}扫描结果` }, { blockquote: "以下文件也许可以安全删除" }, ...markdownContent]))
     }
 
     apply(compiler) {
         compiler.hooks.done.tap(pluginName, (stats) => {
             const safeDeleteFile = this.getFilesDifference(this.allFolderFiles, [...stats.compilation.fileDependencies])
-            fs.writeFileSync('./safeDeleteFile.json', JSON.stringify(safeDeleteFile))
+            this.outputTypeResult(safeDeleteFile)
         })
     }
 }
