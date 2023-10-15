487d75b670184af3a48e23d3acfa1000a46df833
尹县伟 <1605074969@qq.com>
2023-10-15 19:58:03 +0800

    feat: 调整代码

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,5 +1,10 @@
 import json2md from 'json2md';
 import { ModulesAnalysisMarkdownContentType, ModulesUseInfoType } from '../types';
+import type { SFCTemplateCompileResults } from '@vue/compiler-sfc';
+// 这里不使用 require 方式引入会报错，后期处理！
+const template = require('@babel/template').default;
+const traverse = require('@babel/traverse').default;
+const t = require('@babel/types');
 
 export function safeDeleteFileMarkdown(content: Array<string>) {
   const markdownContent = content.map(item => {
@@ -23,4 +28,111 @@ export function modulesAnalysisMarkdown(content: ModulesUseInfoType) {
   });
 
   return json2md(markdownContent);
+}
+
+/**
+ * @description 递归获取 template 中的 click 事件。
+ * @param array
+ * @param events
+ */
+export function recursiveTemplateProps(array: any, events: Array<string> = []) {
+  if (Array.isArray(array)) {
+    array.forEach(item => {
+      if (item.children) {
+        recursiveTemplateProps(item.children, events);
+      }
+      if (item.props) {
+        // 对 props 数组进行操作
+        item.props.forEach((prop: any) => {
+          if (prop.type === 7 && prop.arg.content === 'click') {
+            let eventName = '';
+            if (prop.exp?.children) {
+              eventName = prop.exp?.children[0]?.loc?.source;
+            } else {
+              eventName = prop?.exp?.loc?.source;
+            }
+            events.push(eventName);
+          }
+        });
+      }
+    });
+  }
+}
+
+/**
+ * @description 获取 vue 模版中的 events。
+ * @param templateAst
+ * @returns
+ */
+export function getVueTempllateEvents(templateAst: SFCTemplateCompileResults) {
+  const events: Array<string> = [];
+  recursiveTemplateProps(templateAst.ast?.children, events);
+  return events;
+}
+
+/**
+ * @description 递归处理 ast，并给其添加 log。
+ * @param scriptAst
+ * @param events
+ * @param resourcePath
+ * @returns
+ */
+export function traverseVueScriptAst(scriptAst: any, events: Array<string>, resourcePath: string) {
+  traverse(scriptAst, {
+    BlockStatement(path: any) {
+      if (path.isAddLog) {
+        return;
+      }
+      const parentPath = path?.parentPath;
+      const methdoName = parentPath?.node?.key?.name;
+
+      if (methdoName !== '' && events.includes(methdoName)) {
+        let methodContainsEmit = false;
+        path.traverse({
+          MemberExpression(innerPath: any) {
+            if (
+              t.isThisExpression(innerPath.node.object) &&
+              t.isIdentifier(innerPath.node.property) &&
+              innerPath?.node?.property?.name === '$emit'
+            ) {
+              methodContainsEmit = true;
+            }
+          }
+        });
+
+        const newTemplate = template.ast(`
+          console.log("方法名称: ${methdoName}");
+          console.log("文件位置: ${resourcePath}");
+          console.log("是否触发emit: ${methodContainsEmit ? '是' : '否'}");
+        `);
+
+        path.unshiftContainer('body', newTemplate);
+        path.isAddLog = true;
+      }
+    }
+  });
+
+  return scriptAst;
+}
+
+/**
+ * @description 替换源代码。
+ * @param content
+ * @param replaceContent
+ * @returns
+ */
+export function replaceScriptCode(content: string, replaceContent: string) {
+  let regex = /<script\b([^>]*)>([\s\S]*?)<\/script>/;
+  let matches = content.match(regex);
+
+  if (matches) {
+    let scriptTag = matches[0];
+    let attributes = matches[1];
+    let originalContent = matches[2];
+
+    let newScriptTag = `<script${attributes}>${originalContent.replace(originalContent, replaceContent)}</script>`;
+    content = content.replace(scriptTag, newScriptTag);
+  }
+
+  return content;
 }
--- src/loaders/vueTemplateLog.ts
+++ src/loaders/vueTemplateLog.ts
@@ -1,11 +1,8 @@
 import { LoaderContext } from 'webpack';
 import { parse, compileTemplate } from '@vue/compiler-sfc';
-import { getVueTempllateEvents, replaceScriptCode } from '../utils';
+import { getVueTempllateEvents, replaceScriptCode, traverseVueScriptAst } from '../helpers';
 const parser = require('@babel/parser');
-const traverse = require('@babel/traverse').default;
 const generate = require('@babel/generator').default;
-const template = require('@babel/template').default;
-const t = require('@babel/types');
 /**
  * @description 为 vue template 组件增加 log。
  * 1. click 事件增加 log，方便开发者查询 click 来源。
@@ -30,45 +27,16 @@ export default function VueTemplateLog(this: LoaderContext<any>, source: string)
   });
 
   const events = getVueTempllateEvents(templateAst);
+  if (!events.length) {
+    return source;
+  }
+
   const scriptAst = parser.parse(scriptCode, {
     sourceType: 'unambiguous'
   });
-
-  traverse(scriptAst, {
-    BlockStatement(path: any) {
-      if (path.isAddLog) {
-        return;
-      }
-      const parentPath = path?.parentPath;
-      const methdoName = parentPath?.node?.key?.name;
-
-      if (methdoName !== '' && events.includes(methdoName)) {
-        let methodContainsEmit = false;
-        path.traverse({
-          MemberExpression(innerPath: any) {
-            if (
-              t.isThisExpression(innerPath.node.object) &&
-              t.isIdentifier(innerPath.node.property) &&
-              innerPath?.node?.property?.name === '$emit'
-            ) {
-              methodContainsEmit = true;
-            }
-          }
-        });
-
-        const newTemplate = template.ast(`
-          console.log("方法名称: ${methdoName}");
-          console.log("文件位置: ${resourcePath}");
-          console.log("是否触发emit: ${methodContainsEmit ? '是' : '否'}");
-        `);
-
-        path.unshiftContainer('body', newTemplate);
-        path.isAddLog = true;
-      }
-    }
-  });
-
-  const { code } = generate(scriptAst);
+  // 改方法处理清洗后会给相应的 click 事件增加 log
+  const withLogScriptAst = traverseVueScriptAst(scriptAst, events, resourcePath);
+  const { code } = generate(withLogScriptAst);
   const withLogSource = replaceScriptCode(source, code);
 
   return withLogSource;
